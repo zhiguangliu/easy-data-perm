fdfe394a0bd8c6f8666f73cde78fbb1f185374fa
zhgliu <zhiguangliu1982@gmail.com>
2021-07-31 00:23:35 +0800

    初始化代码

---
+++ easy-data-perm-server/src/main/java/cn/zhgliu/ezdp/EasyDataPermServerApplication.java
@@ -0,0 +1,13 @@
+package cn.zhgliu.ezdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyDataPermServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyDataPermServerApplication.class, args);
+    }
+
+}
---
+++ easy-data-perm-server/src/test/java/cn/zhgliu/ezdp/EasyDataPermServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zhgliu.ezdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyDataPermServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
