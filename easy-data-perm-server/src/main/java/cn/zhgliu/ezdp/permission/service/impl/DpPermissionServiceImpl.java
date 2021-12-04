5545ce7970e3d6b8abc064adfc285a1e9dce54ff
zhgliu <zhiguangliu1982@gmail.com>
2021-12-04 09:46:38 +0800

    创建权限

--- easy-data-perm-server/src/main/java/cn/zhgliu/ezdp/permission/service/impl/DpPermissionServiceImpl.java
+++ easy-data-perm-server/src/main/java/cn/zhgliu/ezdp/permission/service/impl/DpPermissionServiceImpl.java
@@ -1,16 +1,22 @@
 package cn.zhgliu.ezdp.permission.service.impl;
 
 import cn.zhgliu.ezdp.permission.entity.DpPermission;
+import cn.zhgliu.ezdp.permission.entity.DpPermissionItem;
 import cn.zhgliu.ezdp.permission.entity.DpPermissionItemMetadata;
+import cn.zhgliu.ezdp.permission.entity.DpPermissionMetadata;
 import cn.zhgliu.ezdp.permission.mapper.DpPermissionMapper;
 import cn.zhgliu.ezdp.permission.service.IDpPermissionItemMetadataService;
+import cn.zhgliu.ezdp.permission.service.IDpPermissionItemService;
 import cn.zhgliu.ezdp.permission.service.IDpPermissionMetadataService;
 import cn.zhgliu.ezdp.permission.service.IDpPermissionService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -33,19 +39,34 @@ public class DpPermissionServiceImpl extends ServiceImpl<DpPermissionMapper, DpP
     @Resource
     IDpPermissionMetadataService metadataService;
 
+    @Resource
+    IDpPermissionItemService itemService;
+
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public DpPermission addPermission(DpPermission dpPermission) {
         Integer metadataId = dpPermission.getMetadataId();
+
+        DpPermissionMetadata currentMeta = metadataService.getById(metadataId);
+
 
         DpPermission toInsertPermission = new DpPermission();
         toInsertPermission.setMetadataId(metadataId);
         toInsertPermission.setStatus("OK");
-        toInsertPermission.setPermissionName("加个序号");
+        toInsertPermission.setPermissionName(currentMeta.getOperationName()+"-"+ LocalDateTime.now().toString());
         this.baseMapper.insert(toInsertPermission);
 
         DpPermissionItemMetadata param = new DpPermissionItemMetadata();
         param.setPermissionMetadataId(metadataId);
         List<DpPermissionItemMetadata> list = itemMetadataService.list(new QueryWrapper<>(param));
+
+        list.stream().forEach(itemMetaData->{
+            DpPermissionItem item = new DpPermissionItem();
+            BeanUtils.copyProperties(itemMetaData, item);
+            item.setPermissionId(toInsertPermission.getId());
+            item.setMetadataId(metadataId);
+            itemService.save(item);
+        });
 
         System.out.println("===========================");
         System.out.println(toInsertPermission.getId());
